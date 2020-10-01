7472f9e18f14adcfbf6cbc4f665548051133180f
Tanner Noble <tannernoble@gmail.com>
2020-10-01 17:03:15 -0500

    Add routes folder with main routes, setup routes and define them in server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/v1/users', require('./routes/api/v1/users'));
+app.use('/api/v1/auth', require('./routes/api/v1/auth'));
+app.use('/api/v1/profile', require('./routes/api/v1/profile'));
+app.use('/api/v1/properties', require('./routes/api/v1/properties'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
