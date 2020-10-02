95d80607079eab880f68138cb32799af124d932b
Tanner Noble <tannernoble@gmail.com>
2020-10-02 16:47:00 -0500

    Update auth route to authenticate user by email and password (Login).

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -32,7 +32,7 @@ router.post(
 
     try {
       // See if user exists
-      let user = await User.findOne({ email: email });
+      let user = await User.findOne({ email });
 
       if (user) {
         return res
