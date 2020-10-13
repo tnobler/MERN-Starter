f96a0cc53c527593f0321f97e852d1917a941667
Tanner Noble <tannernoble@gmail.com>
2020-10-13 14:14:42 -0500

    Build out Property model and routes file. Created empty Budget model and route files.

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/v1/users', require('./routes/api/v1/users'));
 app.use('/api/v1/auth', require('./routes/api/v1/auth'));
 app.use('/api/v1/profile', require('./routes/api/v1/profile'));
+app.use('/api/v1/properties', require('./routes/api/v1/properties'));
+// app.use('/api/v1/budgets', require('./routes/api/v1/budgets'));
 
 const PORT = process.env.PORT || 5000;
 
